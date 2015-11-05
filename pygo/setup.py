cfb1976d763ec1633f6a1c92dabc955d65db539d
Muhamad Azmy <muhamad.azmy@codescalers.com>
2015-11-05 14:43:57 +0200

    Refactor for PyPi module

---
+++ pygo/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='pygo',
+    packages=['pygo'],  # this must be the same as the name above
+    version='0.1',
+    description='Needed to be installed to use the GO pygo package which allows calling python functions from GO lang',
+    author='Muhamad Azmy',
+    author_email='muhamad.azmy@gmail.com',
+    url='https://github.com/muhamadazmy/pygo',  # use the URL to the github repo
+    download_url='https://github.com/peterldowns/mypackage/tarball/0.1',  # I'll explain this in a second
+    keywords=['pygo', 'golang', 'go'],  # arbitrary keywords
+    classifiers=[],
+)
